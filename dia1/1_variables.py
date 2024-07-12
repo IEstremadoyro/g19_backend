58817d38edb5299aa89ce982e85ebae360baac17
Ignacio Estremadoyro <iestremadoyro@outlook.com.pe>
2024-07-11 21:22:44 -0500

    inicios de python

---
+++ dia1/1_variables.py
@@ -0,0 +1,17 @@
+nombre = "Ignacio"
+edad = 33
+soltero = True
+sueldo = 1025.50
+
+texto1 = "hola como estas"
+texto2 = "Ignacio"
+texto3 = texto1 + texto2
+print(texto3)
+
+alumno1, alumno2, alumno3 = "Ignacio", "Segundo", "Abel"
+print(alumno1)
+print(alumno1.upper())
+
+
+resultado = texto1.split(" ")
+print(resultado)
